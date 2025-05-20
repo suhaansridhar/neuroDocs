0bef3b3275d8eda566863f4d53e22d8b469ea6cf
suhaan sridhar <suhaansridhar@suhaans-MacBook-Pro.local>
2025-05-20 05:48:16 +0530

    styling in login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Check from "./pages/Check"
 import Login from "./pages/Login"
 import { Route, Routes } from "react-router-dom"
 export default function App(){
   return(
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/check" element={<Check />} />
     </Routes>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,37 +2,46 @@ import { useState } from 'react';
 import LoginStyles from './Login.module.css';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 export default function Login(){
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
+    const navigate = useNavigate();
 
     function handleLoginForm(email, password){
         signInWithEmailAndPassword(auth, email, password)
             .then((userCredentials) => {
                 const user = userCredentials.user;
                 console.log("user signed in: ", user.email);
+                navigate("/check")
             })
             .catch((error) => {
-                console.error("Error signing in : ", error.message);
+                alert("Error signing in : ", error.message);
             })
     }
     return (
         <div className={LoginStyles.login__container}>
+            <h3 className={LoginStyles.heading__tag}>NeuroDocs</h3>
             <form className={LoginStyles.login__inner__container} onSubmit={handleLoginForm}>
                 <input 
                     type="email" 
                     placeholder='Enter your username'
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
+                    className={LoginStyles.input__container}
                     />
                 
                 <input 
-                    type="password"
+                    type={showPassword ? "text" : "password"}
                     placeholder='Enter your password'
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
+                    className={LoginStyles.input__container}
                     />
+
+                <button type='button' onClick={() => setShowPassword(prev => !prev)}>eye</button>
                 <button type='submit'>Sign in</button>
             </form>
         </div>
